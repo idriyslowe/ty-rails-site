382d6c1f373763b6e4f855b039c69941a502d7b0
idriyslowe <idriyswins@gmail.com>
2016-12-30 15:41:40 -0600

    new post creation accepts markdown format

--- ty-rails-site/app/helpers/application_helper.rb
+++ ty-rails-site/app/helpers/application_helper.rb
@@ -1,2 +1,6 @@
 module ApplicationHelper
+  def render_markdown_html(text)
+    markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML, quote: true, higlight: true, footnotes: true, hard_wrap: true)
+    return markdown.render(text).html_safe
+  end
 end
